93546fec56289aead7ef66a28ad48cbe3bed5e28
Jonathan Schnabel <jonathan.schnabel31@gmail.com>
2018-12-31 16:20:08 -0500

    update file2db.py

--- sqlitetools/file2db.py
+++ sqlitetools/file2db.py
@@ -22,6 +22,10 @@ def print_help():
     Full path to the database, including the file name. If not provided, the
     database will be named 'new_database.sq3' and placed in the same directory
     as the CSV file.
+
+    -n, --new
+    Boolean value to indicate if a new table should be created from the table
+    name given. Please provide "True" if you want to create the table.
     
     -t, --table
     Name of the table in which to store the CSV file. Must be provided.
@@ -48,8 +52,8 @@ def get_args():
     (with path) for the file and a table name."""
     try:
         opts, args = getopt.getopt(sys.argv[1:], 
-                                   'd:t:f:e:h', 
-                                   ['database=', 'table=', 'file=', 'encoding=', 
+                                   'd:t:n:f:e:h', 
+                                   ['database=', 'table=', 'new=', 'file=', 'encoding=', 
                                     'help'])
 
     except getopt.GetoptError as err:
@@ -63,6 +67,7 @@ def get_args():
     
     db_path = None
     tb_name = None
+    new_table = None
     file_path = None
     encoding = None
     
@@ -74,6 +79,8 @@ def get_args():
             db_path = arg
         elif opt in ('-t', '--t'):
             tb_name = arg
+        elif opt in ('-n', '--new'):
+            new_table = True
         elif opt in ('-f', '--file'):
             file_path = arg
         elif opt in ('-e', '--encoding'):
@@ -89,7 +96,7 @@ def get_args():
         print('Please provide a table name.')
         sys.exit()
     
-    return db_path, tb_name, file_path, encoding
+    return db_path, tb_name, new_table, file_path, encoding
 
 def create_tb_str(field_type, df, tb_name):
     """Return a string that creates a SQLite table when executed by the cursors."""
@@ -118,7 +125,13 @@ def row_to_exec_str(df, row, field_type, tb_name, field_str):
     values = [i for i in df.loc[row, :]]
 
     #add quotation marks to values containing several words
+    # and convert all nan values to NULL
     for index, value in enumerate(values):
+        #convert all nan to NULL in INTEGER and FLOAT columns
+        if field_type[index] != 'TEXT':
+            if math.isnan(value):
+                values[index] = 'NULL'
+
         #avoid trying to modify non-text values
         if field_type[index] == 'TEXT':
             #nan is (weirdly) associated with TEXT but it's a float
@@ -141,7 +154,7 @@ def row_to_exec_str(df, row, field_type, tb_name, field_str):
     
     return exec_str
 
-def file_to_db(db_path=None, tb_name=None, file_path=None, encoding=None):
+def file_to_db(db_path=None, tb_name=None, new_table=None, file_path=None, encoding=None):
     """Function which converts a file to a table in a database."""
     
     #=========================================#
@@ -157,16 +170,22 @@ def file_to_db(db_path=None, tb_name=None, file_path=None, encoding=None):
         sys.exit()
     
     #get working directory as the directory of the file to be put in the database
-    work_dir = '\\'.join(file_path.split('\\')[:-1])
-    
+    system = sys.platform
+    if system == 'win32':
+        work_dir = '\\'.join(file_path.split('\\')[:-1])
+    elif system == 'linux':
+        work_dir = '/'.join(file_path.split('/')[:-1])
     #if no database name was provided, give the database file a general name
     #saved in the same directory as the file to be put in the database
     if db_path == None:
         db_name = 'new_database.sq3'
         db_path = os.path.join(work_dir, db_name)
     else:
-        db_name = db_path.split('\\')[-1]
-    
+        if system == 'win32':
+            db_name = db_path.split('\\')[-1]
+        elif system == 'linux':
+            db_name = db_path.split('/')[-1]
+        
     #create the database if it does not exist already
     if db_name not in next(os.walk(os.getcwd()))[2]:
         #create database file and connect to it, then create cursor
@@ -178,39 +197,46 @@ def file_to_db(db_path=None, tb_name=None, file_path=None, encoding=None):
         cur = conn.cursor()
         
     #determine if the table is already in the database
-    cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
-    for line in cur:
-        if tb_name in line:
-            print("Table name '{}' already used. Please choose another name."\
-                  .format(tb_name))
-            sys.exit()
+    #cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    #for line in cur:
+    #    if tb_name in line:
+    #        print("Table name '{}' already used. Please choose another name."\
+    #              .format(tb_name))
+    #        sys.exit()
 
     #===========================================#
     #=== import csv file to pandas dataframe ===#
     #===========================================#
     
     df = None
-    f_name = file_path.split('\\')[-1]
+    if system == 'win32':
+        f_name = file_path.split('\\')[-1]
+    elif system == 'linux':
+        f_name = file_path.split('/')[-1]
     #if no encoding was provided try utf-8, latin-1 and utf-16
     if encoding == None:
-        try:
-            df = pd.read_csv(file_path, encoding='utf-8')
-            print("Used 'utf-8' to decode the file.")
-        except UnicodeDecodeError:
-            print("Failed to use 'utf-8' to decode the file.")
-            pass
-        try:
-            df = pd.read_csv(file_path, encoding='latin-1')
-            print("Used 'latin-1' to decode the file.")
-        except UnicodeDecodeError:
-            print("Failed to use 'latin-1' to decode the file.")
-            pass
-        try:
-            df = pd.read_csv(file_path, encoding='utf-16')
-            print("Used 'utf-16' to decode the file.")
-        except UnicodeDecodeError:
-            print("Failed to use 'utf-16' to decode the file.")
-            pass
+        while True:
+            try:
+                df = pd.read_csv(file_path, encoding='utf-8')
+                print("Used 'utf-8' to decode the file.")
+                break
+            except UnicodeDecodeError:
+                print("Failed to use 'utf-8' to decode the file.")
+            try:
+                df = pd.read_csv(file_path, encoding='latin-1')
+                print("Used 'latin-1' to decode the file.")
+                break
+            except UnicodeDecodeError:
+                print("Failed to use 'latin-1' to decode the file.")
+            try:
+                df = pd.read_csv(file_path, encoding='utf-16')
+                print("Used 'utf-16' to decode the file.")
+                break
+            except UnicodeDecodeError:
+                print("Failed to use 'utf-16' to decode the file.")
+
+            print('Could not decode file, please select encoding')
+            sys.exit()
     else:
         try:
             df = pd.read_csv(file_path, encoding=encoding)
@@ -229,12 +255,13 @@ def file_to_db(db_path=None, tb_name=None, file_path=None, encoding=None):
     #=== create table in database ===#
     #================================#
 
-    #make executable string to create table in database
-    exec_str_tb = create_tb_str(field_type, df, tb_name)
+    if new_table:
+        #make executable string to create table in database
+        exec_str_tb = create_tb_str(field_type, df, tb_name)
 
-    #create table
-    cur.execute(exec_str_tb)
-    print("Inserted table '{}' in the database.".format(tb_name))
+        #create table
+        cur.execute(exec_str_tb)
+        print("Inserted table '{}' in the database.".format(tb_name))
     
     #============================================#
     #=== put dataframe into table, row by row ===#
@@ -247,19 +274,23 @@ def file_to_db(db_path=None, tb_name=None, file_path=None, encoding=None):
     fields = [i.replace(':', '_').replace('.', '_').replace(' ', '_')\
             .replace('-', '_') for i in df.columns]
     field_str = ('{}, ' * len(fields)).strip(', ').format(*fields)
-    
+
+    #print(field_str)
+
     #get dataframe shape
     nrows, ncols = df.shape
     for row in range(nrows):
         #make executable string
         exec_str_row = row_to_exec_str(df, row, field_type, tb_name, field_str)
-    
+            
+        #print(exec_str_row)
+
         #execute query
         cur.execute(exec_str_row)
 
     #commit work
     conn.commit()
-    print("File '{0}' inserted in the table '{1}' the database '{2}'."\
+    print("File '{0}' inserted in the table '{1}' in the database '{2}'."\
           .format(f_name, tb_name, db_name))
 
     #close cursor and connection to database
@@ -271,5 +302,5 @@ def file_to_db(db_path=None, tb_name=None, file_path=None, encoding=None):
 
 if __name__ == '__main__':
     print('\nRunning file2db...\n')
-    db_path, tb_name, file_path, encoding = get_args()
-    _ = file_to_db(db_path, tb_name, file_path, encoding)
+    db_path, tb_name, new_table, file_path, encoding = get_args()
+    _ = file_to_db(db_path, tb_name, new_table, file_path, encoding)
